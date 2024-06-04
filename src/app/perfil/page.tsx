60863244554820628297d96bcd6b859ae41b8c4b
tiagosan040 <tiagosan040@gmail.com>
2024-06-04 13:21:30 -0300

    feat: add profile page

---
+++ src/app/perfil/page.tsx
@@ -0,0 +1,50 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import businessImage from '@/assets/images/business.png';
+import userImage from '@/assets/images/user.png';
+
+export default function ProfilePage() {
+  return (
+    <main className="w-full flex flex-col items-center -mt-[80px]">
+      <div className="mb-10">
+        <h2 className="font-inter font-semibold text-2xl text-gray-800 text-center">
+          Perfil da Conta
+        </h2>
+        <p className="font-inter font-normal text-sm text-gray-600 text-center">
+          Selecione o perfil da sua conta
+        </p>
+      </div>
+
+      <div className="grid grid-cols-2 gap-6">
+        <Link
+          href="/dono-negocio/entrar"
+          className="flex flex-col items-center gap-4 cursor-pointer px-3 py-4 border-[2px] border-violet-900 rounded-lg">
+          <Image
+            src={{
+              src: businessImage.src,
+              width: 60,
+              height: 60,
+            }}
+            alt="Dono(a) de negócio"
+          />
+          <p className="font-inter font-medium text-base text-violet-900">
+            Dono(a) de negócio
+          </p>
+        </Link>
+        <Link
+          href="/cliente/entrar"
+          className="flex flex-col items-center gap-4 cursor-pointer px-3 py-4 border-[2px] border-violet-900 rounded-lg">
+          <Image
+            src={{
+              src: userImage.src,
+              width: 60,
+              height: 60,
+            }}
+            alt="Cliente"
+          />
+          <p className="font-inter font-medium text-base text-violet-900">Cliente</p>
+        </Link>
+      </div>
+    </main>
+  );
+}
--- src/layouts/header.tsx
+++ src/layouts/header.tsx
@@ -1,12 +1,14 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import { FC } from 'react';
+import LogoImage from '@/assets/images/logo.png';
 
 type HeaderProps = {
-  profile: 'client' | 'owner';
+  profile?: 'client' | 'owner';
+  showButtons?: boolean;
 };
 
-export const Header: FC<HeaderProps> = ({ profile }) => {
+export const Header: FC<HeaderProps> = ({ profile = 'owner', showButtons = true }) => {
   const baseLink = {
     client: '/cliente',
     owner: '/dono-negocio',
@@ -16,26 +18,30 @@ export const Header: FC<HeaderProps> = ({ profile }) => {
 
   return (
     <header className="max-w-[1040px] w-full mx-auto mt-4 flex items-center justify-between">
-      <Image
-        src={{
-          src: 'https://pontoja.s3.amazonaws.com/logo.png',
-          width: 114,
-          height: 40,
-        }}
-        alt="PontoJá"
-      />
-      <div className="flex items-center gap-8">
-        <Link
-          href={accessAccountLink}
-          className="font-inter font-medium text-sm text-gray-700">
-          Acessar conta
-        </Link>
-        <Link
-          href={createAccountLink}
-          className="bg-violet-900 text-white font-inter font-normal text-sm rounded-md py-[6px] px-3 hover:opacity-80 transition-opacity duration-300">
-          Criar conta grátis
-        </Link>
-      </div>
+      <Link href="/perfil">
+        <Image
+          src={{
+            src: LogoImage.src,
+            width: 114,
+            height: 40,
+          }}
+          alt="PontoJá"
+        />
+      </Link>
+      {showButtons && (
+        <div className="flex items-center gap-8">
+          <Link
+            href={accessAccountLink}
+            className="font-inter font-medium text-sm text-gray-700">
+            Acessar conta
+          </Link>
+          <Link
+            href={createAccountLink}
+            className="bg-violet-900 text-white font-inter font-normal text-sm rounded-md py-[6px] px-3 hover:opacity-80 transition-opacity duration-300">
+            Criar conta grátis
+          </Link>
+        </div>
+      )}
     </header>
   );
 };
